7b5252335eced4d5b9e3235cfbdc8a263970b6db
Kaila Burgin <kailaburgin@gmail.com>
2024-06-25 19:59:50 -0400

    project update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Box bg="brand.200" color="brand.500" fontFamily="serif">
+      <Flex
+        justifyContent="space-between"
+        alignContent="center"
+        ml="24px"
+        mr="24px"
+      >
+        <Text as="b" fontSize="5xl">
+          kb
+        </Text>
+        <HStack fontFamily="sans-serif">
+          <Link mr="24px" ml="24px" fontSize="lg">
+            Projects!
+          </Link>
+          <Link mr="24px" ml="24px" fontSize="lg">
+            About
+          </Link>
+          <Link ml="24px" fontSize="lg">
+            Contact
+          </Link>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
